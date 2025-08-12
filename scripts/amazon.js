f8e3fcfab00116d893cea57da8b87c524703c32f
PirovFathullo <pirov1989fathullo@gmail.com>
2025-08-13 00:49:54 +0500

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 
 let productsHTML = "";
 
@@ -60,27 +60,22 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
-    const matchingItem = cart.find((item) => item.productId === productId);
+    addToCart(productId);
 
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-
-    let cartQuantity = 0;
-
-    cart.find((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector(".js-car-quantity").innerHTML = cartQuantity;
+    updateCartQuantity();
   });
 });
